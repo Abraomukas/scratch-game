deae4e99b01b728c1d923d0c558b869bbbdf8335
Abraomukas <abraomukas@gmail.com>
2023-09-17 19:22:04 +0400

    feat: Add config.json
    By default, a 3x3 matrix will be created

--- Makefile
+++ Makefile
@@ -1,11 +1,17 @@
-.PHONY: jar	run
+.PHONY: jar	run play
 
 jar: ## Creates an executable JAR
 	@echo "Creating JAR..."
-	mvn clean
-	java -d sr
+	mvn clean install
+	javac -d bin src/main/java/org/example/Main.java
+	jar cfm Game.jar MANIFEST.MF -C bin .
+	@echo "JAR created!"
 
-run:
+run: ## Runs the JAR
+	@echo "Executing JAR..."
+	mvn clean install
 
-
-# javac src/main/java/org/example/Main.java
+play: ## Runs the Java app
+	@echo "Running the app with the default configuration..."
+	mvn clean install
+	mvn exec:java -Dexec.mainClass="org.example.Main" -Dexec.args="--config config.json --betting-amount 100"
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -9,28 +9,27 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
-//        if (args.length != 4) {
-//            System.out.println("Missing parameters! You need '--config' AND '--betting-amount'");
-//        } else if (!args[0].equals("--config")) {
-//            System.out.println("The first parameter is NOT correct! It has to be '--config'");
-//        } else if (!args[2].equals("--betting-amount")) {
-//            System.out.println("The second parameter is NOT correct! It has to be '--betting-amount'");
-//        } else {
-//        String configPath = args[1];
-        String configPath = "config.json";
+        if (args.length != 4) {
+            System.out.println("Missing parameters! You need '--config' AND '--betting-amount'");
+        } else if (!args[0].equals("--config")) {
+            System.out.println("The first parameter is NOT correct! It has to be '--config'");
+        } else if (!args[2].equals("--betting-amount")) {
+            System.out.println("The second parameter is NOT correct! It has to be '--betting-amount'");
+        } else {
+            String configPath = args[1];
+//            String configPath = "config.json";
 
-        JSONParser jsonParser = new JSONParser();
-        try {
-            JSONObject config = (JSONObject) jsonParser.parse(new FileReader(configPath));
-            Long columns = (Long) config.get("columns");
-            Long rows = (Long) config.get("rows");
+            JSONParser jsonParser = new JSONParser();
+            try {
+                JSONObject config = (JSONObject) jsonParser.parse(new FileReader(configPath));
+                Long columns = (Long) config.get("columns");
+                Long rows = (Long) config.get("rows");
 
-            System.out.println("Hello, world!");
-            System.out.println(columns);
-            System.out.println(rows);
-        } catch (IOException | ParseException e) {
-            throw new RuntimeException(e);
+
+                System.out.println("Creating a " + columns + "x" + rows + " matrix...");
+            } catch (IOException | ParseException e) {
+                throw new RuntimeException(e);
+            }
         }
     }
-//    }
 }
