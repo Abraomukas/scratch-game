d2baae51e90a1fec5279a4034d970750f974be83
Abraomukas <abraomukas@gmail.com>
2023-09-20 23:04:57 +0400

    test: Add unit tests

--- src/test/java/org/example/MainTest.java
+++ src/test/java/org/example/MainTest.java
@@ -1,6 +1,44 @@
 package org.example;
 
-import static org.junit.jupiter.api.Assertions.*;
-class MainTest {
-  
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.Assert.assertTrue;
+
+public class MainTest {
+
+    @Test
+    public void testMainHasFourArgs() {
+        // given
+        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+
+        String[] args = {"--config", "config.json", "--betting-amount", "100"};
+        String expected = "Creating a 3x3 matrix...";
+
+        // when
+        Main.main(args);
+        String capturedOutput = outContent.toString();
+
+        // then
+        assertTrue(capturedOutput.contains(expected));
+    }
+
+    @Test
+    public void testConfigParamMissing() {
+
+    }
+
+    @Test
+    public void testBettingAmountParamMissing() {
+
+    }
+
+    @Test
+    public void testNotWinning() {
+
+    }
+
 }
